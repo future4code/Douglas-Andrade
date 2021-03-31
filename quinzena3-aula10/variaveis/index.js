3b175535de4c11b765b43497c3362c5dff4eab9c
Douglas Andrade <douglasfd@hotmail.com>
2021-03-30 23:51:37 -0300

    Exercicios JavaScript

---
+++ quinzena3-aula10/variaveis/index.js
@@ -0,0 +1,85 @@
+/*  1) Exercicio:
+let a = 10
+let b = 10
+
+console.log(b)
+
+b = 5
+
+console.log(a, b)
+
+Resultado:
+10
+10 5
+
+*/
+
+/* 2) Exercicio: 
+let a = 10
+let b = 10
+let c = a
+let b = c
+let a = b
+
+console.log(a, b, c)
+
+Resultado:
+10 10 10
+*/
+
+// A-1) Exercicio: 
+/*let nome = " ";
+let idade = " ";
+
+console.log(typeof {nome});
+console.log(typeof {idade});
+// Resultado esperado, ambas as variaveis são 'Objeto'. 
+
+nome = prompt ("Seu nome: ");
+idade = prompt ("Sua idade: ");
+
+document.write("Bem-vindo: " + nome + ", tem: " + idade ," anos."); */
+
+//---*-*-*-*-*-*-*-*--
+//A-2) Exercicio:
+
+/*let rua = prompt ("Qual nome da sua rua?");
+document.write ("Rua: ", rua);
+
+let sapato = prompt (" Quanto você calça? ");
+document.write ("Você calça: ", sapato + " ");
+
+let agua = prompt (" bebeu agua hoje ? ");
+document.write ("Você bebeu: ", agua + " Litros de agua ");
+
+let sorvete = prompt ("Sorvete favorito? ");
+document.write ("Seu sorvete favorito ", sorvete + " ");
+
+let time = prompt ("Qual o seu time?");
+document.write ("Você é torcedor do: ", time); */
+
+
+
+//---*-*-*-*-*-*-*-*--
+//A-3) Exercicio:
+/*let comidaFavorita = ["Lasanha", "Pizza", "Hamburguer", "Fritas", "Churrasco"]; */
+
+//console.log(fav);
+/*document.write ("Essas são as minhas comidas preferidas: ", fav);*/
+
+/*fav[1] = prompt ("Qual a sua comida favorita? ");
+console.log(fav);*/
+
+//---*-*-*-*-*-*-*-*--
+//A-4) Exercicio:
+
+
+/*let perguntas = ["Tem instagram? ", "Bebeu Água? ", "Gosta de goiaba? "];
+
+let respostas = ["true", "false"];
+
+console.log(perguntas);
+
+document.write(perguntas); */
+
+//não consegui fazer
